953427db1c32d8ff70a6c3d3526770eea050b1ce
KerstenBreuer <kersten-breuer@outlook.com>
2021-05-18 11:25:34 +0000

    test scripts

---
+++ test.js
@@ -0,0 +1,6 @@
+/*
+test
+*/
+var test = "test2";
+var test_tuple = ["test", 5, true];
+console.log("test: " + test);
---
+++ test.ts
@@ -0,0 +1,6 @@
+/* 
+test 
+*/
+const test: string = "test2";
+const test_tuple: [string, number, boolean] = ["test", 5, true] 
+console.log(`test: ${test}`);
